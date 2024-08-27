e94fb64c468ec46f46135d8b3051fedaa09e7823
gawelszymon <szymon.gawel.szg@gmail.com>
2024-08-27 14:29:54 +0200

    test commit

--- backend/server.py
+++ backend/server.py
@@ -29,4 +29,4 @@ def about():
     return render_template('about.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000, debug=True)
+    app.run(host='0.0.0.0',port=5001, debug=True) #test
