f776b80f6bf155ed44dd3cb03eefaf5a6d4206cb
gawelszymon <szymon.gawel.szg@gmail.com>
2024-10-15 02:11:26 +0200

    update

--- frontend/static/forum_script.js
+++ frontend/static/forum_script.js
@@ -7,7 +7,46 @@ function loadEntries() {
 
             data.forEach(entry => {
 
-                if (entry.entry_type === 'subacademy1' && document.getElementById('entries1')) {
+                if (entry.entry_type === 'main_page' && document.getElementById('entries0')) {
+                    entryType = document.getElementById('entries0');
+
+                    const entriesContainer = entryType;
+
+                    const entryDiv = document.createElement('div');
+                    entryDiv.classList.add('entry');
+
+                    const usernameDiv = document.createElement('div');
+                    usernameDiv.classList.add('username');
+                    usernameDiv.textContent = entry.username;
+
+                    const timestampDiv = document.createElement('div');
+                    timestampDiv.classList.add('timestamp')
+                    timestampDiv.textContent = entry.timestamp;
+
+                    const contentDiv = document.createElement('div');
+                    contentDiv.textContent = entry.content;
+
+                    entryDiv.appendChild(usernameDiv);
+                    entryDiv.appendChild(timestampDiv);     //this variables are visibled bacause being send in data in json
+                    entryDiv.appendChild(contentDiv);
+
+                    if (window.location.pathname === '/addpost_adminaccess_password=lksvistularzaska') {
+                        const entry_typeDiv = document.createElement('div');
+                        entry_typeDiv.textContent = entry.entry_type;
+
+                        const deleteButton = document.createElement('button');
+                        deleteButton.textContent = "Remove";
+                        deleteButton.addEventListener('click', function () {
+                            deleteEntry(entry.id);
+                        });
+
+                        entryDiv.appendChild(entry_typeDiv);
+                        entryDiv.appendChild(deleteButton);
+                    }
+
+                    entriesContainer.appendChild(entryDiv);
+
+                } else if (entry.entry_type === 'subacademy1' && document.getElementById('entries1')) {
                     entryType = document.getElementById('entries1');
 
                     const entriesContainer = entryType;
@@ -89,6 +128,252 @@ function loadEntries() {
                     entriesContainer.appendChild(entryDiv);
                 } else if (entry.entry_type === 'subacademy3' && document.getElementById('entries3')) {
                     entryType = document.getElementById('entries3');
+
+                    const entriesContainer = entryType;
+
+                    const entryDiv = document.createElement('div');
+                    entryDiv.classList.add('entry');
+
+                    const usernameDiv = document.createElement('div');
+                    usernameDiv.classList.add('username');
+                    usernameDiv.textContent = entry.username;
+
+                    const timestampDiv = document.createElement('div');
+                    timestampDiv.classList.add('timestamp')
+                    timestampDiv.textContent = entry.timestamp;
+
+                    const contentDiv = document.createElement('div');
+                    contentDiv.textContent = entry.content;
+
+                    const entry_typeDiv = document.createElement('div');
+                    entry_typeDiv.textContent = entry.entry_type;
+
+                    entryDiv.appendChild(usernameDiv);
+                    entryDiv.appendChild(timestampDiv);     //this variables are visibled bacause being send in data in json
+                    entryDiv.appendChild(contentDiv);
+
+                    if (window.location.pathname === '/addpost_adminaccess_password=lksvistularzaska') {
+                        const entry_typeDiv = document.createElement('div');
+                        entry_typeDiv.textContent = entry.entry_type;
+
+                        const deleteButton = document.createElement('button');
+                        deleteButton.textContent = "Remove";
+                        deleteButton.addEventListener('click', function () {
+                            deleteEntry(entry.id);
+                        });
+
+                        entryDiv.appendChild(entry_typeDiv);
+                        entryDiv.appendChild(deleteButton);
+                    }
+
+                    entriesContainer.appendChild(entryDiv);
+                } else if (entry.entry_type === 'subacademy4' && document.getElementById('entries4')) {
+                    entryType = document.getElementById('entries4');
+
+                    const entriesContainer = entryType;
+
+                    const entryDiv = document.createElement('div');
+                    entryDiv.classList.add('entry');
+
+                    const usernameDiv = document.createElement('div');
+                    usernameDiv.classList.add('username');
+                    usernameDiv.textContent = entry.username;
+
+                    const timestampDiv = document.createElement('div');
+                    timestampDiv.classList.add('timestamp')
+                    timestampDiv.textContent = entry.timestamp;
+
+                    const contentDiv = document.createElement('div');
+                    contentDiv.textContent = entry.content;
+
+                    const entry_typeDiv = document.createElement('div');
+                    entry_typeDiv.textContent = entry.entry_type;
+
+                    entryDiv.appendChild(usernameDiv);
+                    entryDiv.appendChild(timestampDiv);     //this variables are visibled bacause being send in data in json
+                    entryDiv.appendChild(contentDiv);
+
+                    if (window.location.pathname === '/addpost_adminaccess_password=lksvistularzaska') {
+                        const entry_typeDiv = document.createElement('div');
+                        entry_typeDiv.textContent = entry.entry_type;
+
+                        const deleteButton = document.createElement('button');
+                        deleteButton.textContent = "Remove";
+                        deleteButton.addEventListener('click', function () {
+                            deleteEntry(entry.id);
+                        });
+
+                        entryDiv.appendChild(entry_typeDiv);
+                        entryDiv.appendChild(deleteButton);
+                    }
+
+                    entriesContainer.appendChild(entryDiv);
+                } else if (entry.entry_type === 'subacademy5' && document.getElementById('entries5')) {
+                    entryType = document.getElementById('entries5');
+
+                    const entriesContainer = entryType;
+
+                    const entryDiv = document.createElement('div');
+                    entryDiv.classList.add('entry');
+
+                    const usernameDiv = document.createElement('div');
+                    usernameDiv.classList.add('username');
+                    usernameDiv.textContent = entry.username;
+
+                    const timestampDiv = document.createElement('div');
+                    timestampDiv.classList.add('timestamp')
+                    timestampDiv.textContent = entry.timestamp;
+
+                    const contentDiv = document.createElement('div');
+                    contentDiv.textContent = entry.content;
+
+                    const entry_typeDiv = document.createElement('div');
+                    entry_typeDiv.textContent = entry.entry_type;
+
+                    entryDiv.appendChild(usernameDiv);
+                    entryDiv.appendChild(timestampDiv);     //this variables are visibled bacause being send in data in json
+                    entryDiv.appendChild(contentDiv);
+
+                    if (window.location.pathname === '/addpost_adminaccess_password=lksvistularzaska') {
+                        const entry_typeDiv = document.createElement('div');
+                        entry_typeDiv.textContent = entry.entry_type;
+
+                        const deleteButton = document.createElement('button');
+                        deleteButton.textContent = "Remove";
+                        deleteButton.addEventListener('click', function () {
+                            deleteEntry(entry.id);
+                        });
+
+                        entryDiv.appendChild(entry_typeDiv);
+                        entryDiv.appendChild(deleteButton);
+                    }
+
+                    entriesContainer.appendChild(entryDiv);
+                } else if (entry.entry_type === 'subacademy6' && document.getElementById('entries6')) {
+                    entryType = document.getElementById('entries6');
+
+                    const entriesContainer = entryType;
+
+                    const entryDiv = document.createElement('div');
+                    entryDiv.classList.add('entry');
+
+                    const usernameDiv = document.createElement('div');
+                    usernameDiv.classList.add('username');
+                    usernameDiv.textContent = entry.username;
+
+                    const timestampDiv = document.createElement('div');
+                    timestampDiv.classList.add('timestamp')
+                    timestampDiv.textContent = entry.timestamp;
+
+                    const contentDiv = document.createElement('div');
+                    contentDiv.textContent = entry.content;
+
+                    const entry_typeDiv = document.createElement('div');
+                    entry_typeDiv.textContent = entry.entry_type;
+
+                    entryDiv.appendChild(usernameDiv);
+                    entryDiv.appendChild(timestampDiv);     //this variables are visibled bacause being send in data in json
+                    entryDiv.appendChild(contentDiv);
+
+                    if (window.location.pathname === '/addpost_adminaccess_password=lksvistularzaska') {
+                        const entry_typeDiv = document.createElement('div');
+                        entry_typeDiv.textContent = entry.entry_type;
+
+                        const deleteButton = document.createElement('button');
+                        deleteButton.textContent = "Remove";
+                        deleteButton.addEventListener('click', function () {
+                            deleteEntry(entry.id);
+                        });
+
+                        entryDiv.appendChild(entry_typeDiv);
+                        entryDiv.appendChild(deleteButton);
+                    }
+
+                    entriesContainer.appendChild(entryDiv);
+                } else if (entry.entry_type === 'subacademy7' && document.getElementById('entries7')) {
+                    entryType = document.getElementById('entries7');
+
+                    const entriesContainer = entryType;
+
+                    const entryDiv = document.createElement('div');
+                    entryDiv.classList.add('entry');
+
+                    const usernameDiv = document.createElement('div');
+                    usernameDiv.classList.add('username');
+                    usernameDiv.textContent = entry.username;
+
+                    const timestampDiv = document.createElement('div');
+                    timestampDiv.classList.add('timestamp')
+                    timestampDiv.textContent = entry.timestamp;
+
+                    const contentDiv = document.createElement('div');
+                    contentDiv.textContent = entry.content;
+
+                    const entry_typeDiv = document.createElement('div');
+                    entry_typeDiv.textContent = entry.entry_type;
+
+                    entryDiv.appendChild(usernameDiv);
+                    entryDiv.appendChild(timestampDiv);     //this variables are visibled bacause being send in data in json
+                    entryDiv.appendChild(contentDiv);
+
+                    if (window.location.pathname === '/addpost_adminaccess_password=lksvistularzaska') {
+                        const entry_typeDiv = document.createElement('div');
+                        entry_typeDiv.textContent = entry.entry_type;
+
+                        const deleteButton = document.createElement('button');
+                        deleteButton.textContent = "Remove";
+                        deleteButton.addEventListener('click', function () {
+                            deleteEntry(entry.id);
+                        });
+
+                        entryDiv.appendChild(entry_typeDiv);
+                        entryDiv.appendChild(deleteButton);
+                    }
+
+                    entriesContainer.appendChild(entryDiv);
+                } else if (entry.entry_type === 'subacademy8' && document.getElementById('entries8')) {
+                    entryType = document.getElementById('entries8');
+
+                    const entriesContainer = entryType;
+
+                    const entryDiv = document.createElement('div');
+                    entryDiv.classList.add('entry');
+
+                    const usernameDiv = document.createElement('div');
+                    usernameDiv.classList.add('username');
+                    usernameDiv.textContent = entry.username;
+
+                    const timestampDiv = document.createElement('div');
+                    timestampDiv.classList.add('timestamp')
+                    timestampDiv.textContent = entry.timestamp;
+
+                    const contentDiv = document.createElement('div');
+                    contentDiv.textContent = entry.content;
+
+                    const entry_typeDiv = document.createElement('div');
+                    entry_typeDiv.textContent = entry.entry_type;
+
+                    entryDiv.appendChild(usernameDiv);
+                    entryDiv.appendChild(timestampDiv);     //this variables are visibled bacause being send in data in json
+                    entryDiv.appendChild(contentDiv);
+
+                    if (window.location.pathname === '/addpost_adminaccess_password=lksvistularzaska') {
+                        const entry_typeDiv = document.createElement('div');
+                        entry_typeDiv.textContent = entry.entry_type;
+
+                        const deleteButton = document.createElement('button');
+                        deleteButton.textContent = "Remove";
+                        deleteButton.addEventListener('click', function () {
+                            deleteEntry(entry.id);
+                        });
+
+                        entryDiv.appendChild(entry_typeDiv);
+                        entryDiv.appendChild(deleteButton);
+                    }
+
+                    entriesContainer.appendChild(entryDiv);
+                } else if (entry.entry_type === 'subacademy9' && document.getElementById('entries9')) {
+                    entryType = document.getElementById('entries9');
 
                     const entriesContainer = entryType;
 
