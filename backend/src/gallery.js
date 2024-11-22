3516fe5c9dab907cd3f202e9e18f60bf756f1d72
gawelszymon <szymon.gawel.szg@gmail.com>
2024-11-22 20:32:29 +0100

    react configured

---
+++ backend/src/gallery.js
@@ -0,0 +1,6 @@
+class WhateverYouWant extends React.Component {
+    render() {
+        return <div>Hello from React!</div>;
+    }
+}
+ReactDOM.render(<WhateverYouWant />, document.getElementById("root"));
